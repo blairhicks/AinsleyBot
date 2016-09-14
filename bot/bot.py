d5f3577a55cfb1c878de3716b66ac03a2613af90
Blair Hicks <blairhicks@nowhere.net>
2016-09-13 20:39:30 -0500

    More debug

--- bot/bot.py
+++ bot/bot.py
@@ -194,8 +194,11 @@ def stop_room(incoming):
 
 # Construct a help message for users.
 def send_help(post_data):
+    # Say hello
+    if DEBUG:
+        sys.stderr.write('*** inside send_help ***')
     message = "Hello!  "
-    message = message + "Updated at 4:09pm  \n"
+    message = message + "Updated at 13Sept 8:38pm  \n"
     message = message + "I understand the following commands:  \n"
     for c in commands.items():
         message = message + "* **%s**: %s \n" % (c[0], c[1])
